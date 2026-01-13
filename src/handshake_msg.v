3175cc7df0eae479463c18ab67ffba8dccc01b66
blackshirt <blackshirt@users.noreply.github.com>
2026-01-13 14:00:08 +0000

    updates hsk test

--- src/handshake_msg.v
+++ src/handshake_msg.v
@@ -966,6 +966,17 @@ fn size_fin(f Finished) int {
 	return f.verify_data.len
 }
 
+// finished_from_hsk tries to create Finished message from raw Handshake message
+@[inline]
+fn finished_from_hsk(h Handshake) !Finished {
+	if h.tipe != .finished {
+		return error('not finished message')
+	}
+	return Finished{
+		verify_data: h.payload
+	}
+}
+
 // 4.6.1.  New Session Ticket Message
 //
 const min_nst_size = 13
@@ -996,11 +1007,9 @@ fn size_nst(st NewSessionTicket) int {
 	mut n := 0
 	n += 8 // ticket lifetime + ageadd
 	// 1-byte nonce.len and the nonce
-	n += 1
-	n += st.nonce.len
+	n += 1 + st.nonce.len
 	// 2-bytes ticket.len and the ticket
-	n += 2
-	n += st.ticket.len
+	n += 2 + st.ticket.len
 
 	// extension list with 2-bytes length
 	n += size_extlist(st.xslist, .size2)
@@ -1011,7 +1020,7 @@ fn size_nst(st NewSessionTicket) int {
 // check_nst does basic validation on NewSessionTicket message st
 @[inline]
 fn (st NewSessionTicket) check_nst() ! {
-	return error('TODO')
+	// TODO
 }
 
 // pack_nst encodes NewSessionTicket message st into bytes array.
@@ -1046,16 +1055,18 @@ fn parse_nst(b []u8) !NewSessionTicket {
 	}
 	mut r := new_buffer(b)!
 
+	// parse ticket lifetime and ageadd
 	lifetime := r.read_u32()!
 	ageadd := r.read_u32()!
 
+	// parse 1-byte nonce length and the nonce bytes
 	nonce_len := r.read_u8()!
 	nonce := r.read_at_least(int(nonce_len))!
-
+	// parse 2-bytes ticket length and the ticket bytes
 	tkt_len := r.read_u16()!
 	ticket := r.read_at_least(int(tkt_len))!
 
-	// read extension list with prepended length
+	// read extension list with 2-bytes length
 	xlen := r.read_u16()!
 	xs_bytes := r.read_at_least(int(xlen))!
 	xs := parse_extlist_nolen(xs_bytes)!
